8722a531c6b86bf70c0360f8ff3315bc1002d879
Diego Monti <diegmonti@gmail.com>
2018-07-17 12:14:40 +0200

    Save output to log file

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 source venv/bin/activate
 
-gunicorn -b 127.0.0.1:5000 main:app &
+gunicorn -b 127.0.0.1:5000 main:app &>> log.txt &
 
 cd recommenders
 
-gunicorn -b 127.0.0.1:5001 random_rec:app &
-gunicorn -b 127.0.0.1:5002 most_popular:app &
-gunicorn -b 127.0.0.1:5003 itemknn:app &
-gunicorn -b 127.0.0.1:5004 userknn:app &
-gunicorn -b 127.0.0.1:5005 bprmf:app &
-gunicorn -b 127.0.0.1:5006 wrmf:app &
+gunicorn -b 127.0.0.1:5001 random_rec:app &>> log.txt &
+gunicorn -b 127.0.0.1:5002 most_popular:app &>> log.txt &
+gunicorn -b 127.0.0.1:5003 itemknn:app &>> log.txt &
+gunicorn -b 127.0.0.1:5004 userknn:app &>> log.txt &
+gunicorn -b 127.0.0.1:5005 bprmf:app &>> log.txt &
+gunicorn -b 127.0.0.1:5006 wrmf:app &>> log.txt &
