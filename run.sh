ea9889ba36917a4c468812f3888a147d87d7dcaa
Diego Monti <diegmonti@gmail.com>
2018-07-18 16:33:20 +0200

    Fix log file

--- run.sh
+++ run.sh
@@ -6,9 +6,10 @@ gunicorn -b 127.0.0.1:5000 main:app &>> log.txt &
 
 cd recommenders
 
-gunicorn -b 127.0.0.1:5001 random_rec:app &>> log.txt &
-gunicorn -b 127.0.0.1:5002 most_popular:app &>> log.txt &
-gunicorn -b 127.0.0.1:5003 itemknn:app &>> log.txt &
-gunicorn -b 127.0.0.1:5004 userknn:app &>> log.txt &
-gunicorn -b 127.0.0.1:5005 bprmf:app &>> log.txt &
-gunicorn -b 127.0.0.1:5006 wrmf:app &>> log.txt &
+gunicorn -b 127.0.0.1:5001 random_rec:app &>> ../log.txt &
+gunicorn -b 127.0.0.1:5002 most_popular:app &>> ../log.txt &
+gunicorn -b 127.0.0.1:5003 itemknn:app &>> ../log.txt &
+gunicorn -b 127.0.0.1:5004 userknn:app &>> ../log.txt &
+gunicorn -b 127.0.0.1:5005 bprmf:app &>> ../log.txt &
+gunicorn -b 127.0.0.1:5006 wrmf:app &>> ../log.txt &
+
